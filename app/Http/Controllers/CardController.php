1d409d6732d31d55840f9898c2faca9b88a8c346
jycurien <curien.jeanyves@gmail.com>
2022-03-20 10:31:32 +0100

    find card by code

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -4,13 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Models\Card;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use InvalidArgumentException;
 
 class CardController extends Controller
 {
     const LIMIT_CARDS_TO_DISPLAY = 20;
 
+    public function findByCode($code) {
+        if (10 !== strlen($code)) {
+            throw new InvalidArgumentException('Invalid card number');
+        }
+        [$centerCode, $cardCode, $checkSum] = $this->extractCodeParts($code);
+        return Card::where('center_code', $centerCode)
+                ->where('card_code', $cardCode)
+                ->where('check_sum', $checkSum)
+                ->firstOrFail();
+    }
+
     public function index()
     {
         return Card::orderBy('id', 'DESC')->take(self::LIMIT_CARDS_TO_DISPLAY)->get();
+    }
+
+    /**
+     * @param $code
+     * @return array
+     */
+    private function extractCodeParts($code): array
+    {
+        return [substr($code, 0, 3), substr($code, 3, 6), substr($code, 9)];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/cards/code-{code}', [CardController::class, 'findByCode'])->where('code', '\d+');;
 Route::get('/cards', [CardController::class, 'index']);
